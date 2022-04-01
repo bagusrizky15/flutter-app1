2ca71c8a4cce5aa4415129a7157c25306b625e80
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2022-04-01 09:33:25 +0700

    remove debug

--- lib/main.dart
+++ lib/main.dart
@@ -1,2 +1,23 @@
-import 'package:flutter/material.dart'
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Text("Halo"),
+        ),
+        appBar: AppBar(
+          title: Text("My First App"),
+        ),
+      ),
+    );
+  }
+}
