d46dc4b38e0e93808706bfea36029dc6b4011b1b
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2022-04-02 07:38:42 +0700

    listview

--- lib/main.dart
+++ lib/main.dart
@@ -5,30 +5,32 @@ void main(List<String> args) {
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Column"),
+          title: Text("ListView"),
         ),
-        body: Stack(
+        body: ListView(
           children: [
             Container(
-              width: 400,
-              height: 400,
-              color: Colors.green,
-            ),
-            Container(
-              width: 300,
-              height: 300,
+              width: 200,
+              height: 200,
               color: Colors.blue,
             ),
             Container(
               width: 200,
               height: 200,
               color: Colors.red,
-            )
+            ),
+            Container(
+              width: 200,
+              height: 300,
+              color: Colors.amber,
+            ),
           ],
         ),
       ),
