6f8743ead6686a9e13c87576fe25a0ddc76e9b70
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2022-04-02 12:12:42 +0700

    make list tile

--- lib/main.dart
+++ lib/main.dart
@@ -5,25 +5,12 @@ void main(List<String> args) {
 }
 
 class MyApp extends StatelessWidget {
-  final List<Widget> myList = List.generate(
-    100,
-    (index) => Text(
-      "${index + 1}",
-      style: TextStyle(
-        fontSize: 20 + double.parse(index.toString()),
-      ),
-    ),
-  );
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("ListView"),
-        ),
-        body: ListView(
-          children: myList,
+          title: Text("hola"),
         ),
       ),
     );
